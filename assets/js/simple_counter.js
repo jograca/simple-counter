c46c9e6ee6bb185a85dc1ead7359ddf12a95344c
Jon Graca <jon.graca@gmail.com>
2017-12-18 12:49:57 -0500

    Adding in JS Counter loops

--- assets/js/simple_counter.js
+++ assets/js/simple_counter.js
@@ -1,7 +1,35 @@
 (function() {
 
-    console.log("Javascript is Running");
+    const incrementButton = document.querySelector("#incrementButton");
 
+    const decrementButton = document.querySelector("#decrementButton");
     
-})() // brackets at the end indicate that the fuction should be run
-    // when page loads
+    const counter = document.querySelector("#counter");
+
+    incrementButton.addEventListener("click", function() {
+        if(parseInt(counter.innerHTML) >= 9) {
+            counter.innerHTML = parseInt(counter.innerHTML) +1; 
+            counter.style.color = "red";
+        }
+        else {
+            counter.innerHTML = parseInt(counter.innerHTML) +1;
+        }
+    });
+
+    decrementButton.addEventListener("click", function() {
+        if(parseInt(counter.innerHTML) <= 0) {
+            counter.innerHTML = 1; 
+        }
+        if(parseInt(counter.innerHTML) <= 10) {
+            console.log("LT 11");
+            counter.innerHTML = parseInt(counter.innerHTML) -1;
+            counter.style.color = "black";  
+        }
+        if(parseInt(counter.innerHTML) >= 11) {
+            console.log("GT 11");
+            counter.innerHTML = parseInt(counter.innerHTML) -1;
+            counter.style.color = "red";  
+        };
+    });
+
+})() 
